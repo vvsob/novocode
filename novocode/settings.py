f27d6d4276d728c8923fe8c5434b9ecdf2c8b0dd
lerasimus <lerasimmus229@gmail.com>
2023-11-26 01:31:47 +0700

    update .env location

--- novocode/settings.py
+++ novocode/settings.py
@@ -14,11 +14,10 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
