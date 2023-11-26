fabcee37fdf75a97112033c7f66d46503b56635f
lerasimus <lerasimmus229@gmail.com>
2023-11-26 16:31:43 +0700

    make compile command nullable and blankable

--- compilers/models.py
+++ compilers/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Compiler(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256, null=True)
-    compile_command = models.CharField(max_length=1024)
+    name = models.CharField(max_length=256)
+    compile_command = models.CharField(max_length=1024, blank=True, null=True)
     run_command = models.CharField(max_length=1024)
     file_extension = models.CharField(max_length=16)
 
