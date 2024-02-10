95e722e9201dd4f5a71f8e3df088796d3889f9e8
Oleg Sobolev <lerasimmus229@gmail.com>
2024-02-10 20:48:41 +0700

    make testing verdict show current test

--- format_verdict/common.py
+++ format_verdict/common.py
@@ -1,5 +1,7 @@
-def format_testing(verdict):
-    return """<span class="text-primary">Testing</span>"""
+def format_testing(verdict: dict):
+    if "current_test" not in verdict:
+        return """<span class="text-primary">Testing</span>"""
+    return f"""<span class="text-primary">Testing {verdict["current_test"]}</span>"""
 
 
 def format_judge_error(verdict):
